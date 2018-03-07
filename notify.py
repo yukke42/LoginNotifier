3aa63aefd769515a4c8debe51e912a4f7289e06e
yukke42 <muramatsuyusuke9542@gmail.com>
2018-03-07 23:05:31 +0900

    Add: 通知しないIPリストを設定できるように

--- notify.py
+++ notify.py
@@ -5,16 +5,26 @@ import sys
 import requests
 import yaml
 
+with open('config.yml') as f:
+    config = yaml.load(f)
+
+WEBHOOK_URL = config['webhook_url']
+IGNORE_IP_LIST = config.get('ignore_ip_list', [])
+
 
 def get_hostip():
     resp = requests.get('http://inet-ip.info/ip')
     return resp.content.decode()
 
 
-def notify(url):
+def notify():
     user = sys.argv[1]
     client_ip = sys.argv[2].split(' ')[0]
     host_ip = get_hostip()
+
+    if client_ip in IGNORE_IP_LIST:
+        return
+
     data = {
         'username':
         'LoginNotifier',
@@ -22,16 +32,8 @@ def notify(url):
         '<!channel> {} から {} に ユーザー {} でログインしました！'.format(
             client_ip, host_ip, user)
     }
-    requests.post(url, data=json.dumps(data))
-
-
-def main():
-    with open('config.yml', 'r') as f:
-        config = yaml.load(f)
-
-    webhook_url = config['webhook_url']
-    notify(webhook_url)
+    requests.post(WEBHOOK_URL, data=json.dumps(data))
 
 
 if __name__ == '__main__':
-    main()
+    notify()
