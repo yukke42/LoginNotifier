99288604b49b5ade40aa7a2e38705345469ad045
yukke42 <muramatsuyusuke9542@gmail.com>
2018-03-07 22:50:10 +0900

    Refacotr: PEP8を満たすように修正

--- notify.py
+++ notify.py
@@ -1,17 +1,26 @@
-import requests, json, yaml, sys, argparse
+import argparse
+import json
+import sys
+
+import requests
+import yaml
 
 
 def get_hostip():
     resp = requests.get('http://inet-ip.info/ip')
     return resp.content.decode()
 
+
 def notify(url):
     user = sys.argv[1]
     client_ip = sys.argv[2].split(' ')[0]
     host_ip = get_hostip()
     data = {
-        'username': 'LoginNotifier',
-        'text': '<!channel> {} から {} に ユーザー {} でログインしました！'.format(client_ip, host_ip, user)
+        'username':
+        'LoginNotifier',
+        'text':
+        '<!channel> {} から {} に ユーザー {} でログインしました！'.format(
+            client_ip, host_ip, user)
     }
     requests.post(url, data=json.dumps(data))
 
